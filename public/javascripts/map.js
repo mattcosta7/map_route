b2cb5e0fa1491032723de6ecc39563bdefecb595
Matthew Costabile <mattcosta7@gmail.com>
2016-04-25 10:01:53 -0400

    tidy

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -6,7 +6,7 @@ function initMap() {
       var origin = {lat: response.lat1, lng: response.lng1};
       var destination = {lat: response.lat2, lng: response.lng2};
       var distanceTraveled = response.distance_traveled;
-      
+
       map = new google.maps.Map(document.getElementById('map'), {});
       infowindow = new google.maps.InfoWindow();
       placeMarker(origin,"origin");
@@ -20,19 +20,19 @@ function initMap() {
 }
 
 function calculateAndDisplayRoute(origin, destination,distanceTraveled) {
-  var directionsService = new google.maps.DirectionsService;
+  var directionsService = new google.maps.DirectionsService();
   var routeOptions = {
     origin: origin,
     destination: destination,
     travelMode: google.maps.TravelMode.DRIVING
-  }
+  };
   directionsService.route(routeOptions, function(response, status) {
     if (status === google.maps.DirectionsStatus.OK) {
-      var totalRouteDistance = response.routes[0].legs[0].distance
+      var totalRouteDistance = response.routes[0].legs[0].distance;
       updateDisplay(totalRouteDistance, distanceTraveled);
       var steps = response.routes[0].legs[0].steps;
       plotMap(steps, distanceTraveled);
-    } 
+    }
     else {
       window.alert('Directions request failed due to ' + status);
     }
@@ -70,9 +70,9 @@ function plotMap(steps,distanceTraveled){
     var nextPathPart = steps[j].path;
     for (k=0;k<nextPathPart.length;k++) {
       if(k<nextPathPart.length-1){
-        var path_origin = nextPathPart[k]
-        var dest_path = nextPathPart[k+1]
-        var distToCover = google.maps.geometry.spherical.computeDistanceBetween(path_origin,dest_path)
+        var path_origin = nextPathPart[k];
+        var dest_path = nextPathPart[k+1];
+        var distToCover = google.maps.geometry.spherical.computeDistanceBetween(path_origin,dest_path);
         if(distanceCovered > distanceTraveled){
           polylineFromPoint.getPath().push(nextPathPart[k]);
         }
@@ -106,7 +106,7 @@ function plotMap(steps,distanceTraveled){
   map.fitBounds(bounds);
 }
 
-function placeMarker(location,type,dist=null){
+function placeMarker(location,type,dist){
   if(type=="point"){
     options = {
       icon: {
@@ -115,19 +115,19 @@ function placeMarker(location,type,dist=null){
         origin: new google.maps.Point(0, 0),
         anchor: new google.maps.Point(0, 32)
       },
-      title: "Point " + String(dist) + " meters from origin",
-    }
+      title: "Point traveled to",
+    };
   }
   else{
     options = {
       label: type[0],
       title: type
-    }
+    };
   }
   var marker = new google.maps.Marker({
     position: location,
     map: map,
-    
+
     animation: google.maps.Animation.DROP,
     options
   });
@@ -142,6 +142,6 @@ function placeMarker(location,type,dist=null){
 function updateDisplay(totalDist, distanceTraveled){
   document.getElementById('distance_placeholder').innerHTML = totalDist.value;
   document.getElementById('distance_placeholder_2').innerHTML = Math.round(getMiles((totalDist.value)*10))/10 + " miles";
-  document.getElementById('distance_to_travel').innerHTML = totalDist.value > distanceTraveled ? (totalDist.value - distanceTraveled) : 'nil';
+  document.getElementById('distance_to_travel').innerHTML = totalDist.value > distanceTraveled ? Math.round((totalDist.value - distanceTraveled)*10)/10 : 'nil';
   document.getElementById('distance_to_travel_2').innerHTML = (totalDist.value > distanceTraveled ? (Math.round(getMiles((totalDist.value - distanceTraveled)*10))/10 + " miles") : "passed your destination, dude");
-};
+}
